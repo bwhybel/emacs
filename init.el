ee2cac2c54ebdff1a8968b3b2572ead426dd7a79
bweibel <bweibel@belvederetrading.com>
2019-11-12 11:09:21 -0600

    [111219] Making sure company works and adding git
    Add in Git Gutter and add stuff for Company

--- init.el
+++ init.el
@@ -3,8 +3,8 @@
 
 (require 'package)
 (setq package-enable-at-startup nil)
-(add-to-list 'package-archives
-             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
